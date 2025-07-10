151f7f2a98304c35185012a7d0038a41a349535a
Carlos Espinoza <c.augusto.espinoza@gmail.com>
2025-07-09 22:33:53 -0500

    correcion de video

--- src/app/(frontend)/[marca]/page.tsx
+++ src/app/(frontend)/[marca]/page.tsx
@@ -138,7 +138,7 @@ export default function Marca() {
         slugMarca: '/gloria/',
         slug: '/gloria/',
         type: 'video',
-        multimedia: '/bhome.webp',
+        multimedia: '/videocorto.mp4',
         like: '32',
         duracion: '23 seg',
         logoMarca: '/gloriaMarca.svg',
